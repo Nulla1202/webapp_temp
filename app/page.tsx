b285c380ef91438177cdc4579b3cb420a621da6c
Nulla1202 <daistar1202@gmail.com>
2025-03-21 07:40:28 +0900

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Page() {
+  return <main>
+    <p>私の得意な言語を以下に示します</p>
+    <ul>
+        <li>Python</li>
+        <li>Typescript</li>
+        <li>MATLAB</li>
+    </ul>
+  </main>
+}
